03c50c73273aa85546d59a95da894f8f791f722e
fernando <fcechett@gmail.com>
2021-06-01 12:49:06 -0300

    Aula 4 - Clean_field

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,9 +1,11 @@
+from typing import OrderedDict
 from django import forms
 from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from passagens.classe_viagem import tipos_de_classe
 
 class PassagemForms(forms.Form):
+    
     origem = forms.CharField(label='Origem', max_length=100)
     destino = forms.CharField(label='Destino', max_length=100)
     data_ida = forms.DateField(label='Ida',widget=DatePicker())
@@ -16,5 +18,12 @@ class PassagemForms(forms.Form):
         widget=forms.Textarea(),
         required=False
     )
+    
     email = forms.EmailField(label='Email', max_length=150)
     
+    def clean_origem(self):
+        origem = self.cleaned_data.get('origem')
+        if any(char.isdigit() for char in origem):
+            raise forms.ValidationError('Origem inválida: Não incluir número!')
+        else:
+            return origem
