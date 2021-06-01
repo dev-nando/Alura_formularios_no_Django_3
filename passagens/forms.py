133858cb7e5e64ee28d40b0ad9ab061c4a91c79e
fernando <fcechett@gmail.com>
2021-06-01 13:18:11 -0300

    Aula 4 - Validando datas

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,5 +1,5 @@
-from typing import OrderedDict
 from django import forms
+
 from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from passagens.classe_viagem import tipos_de_classe
@@ -21,17 +21,23 @@ class PassagemForms(forms.Form):
     )
     
     email = forms.EmailField(label='Email', max_length=150)
-    
+
     def clean(self):
         
         origem = self.cleaned_data.get('origem')
         destino = self.cleaned_data.get('destino')
+        data_ida = self.cleaned_data.get('data_ida')
+        data_volta = self.cleaned_data.get('data_volta')
+        data_pesquisa = self.cleaned_data.get('data_pesquisa')
         lista_de_erros = {}
         campo_tem_algum_numero(origem, 'origem', lista_de_erros)
         campo_tem_algum_numero(destino, 'destino', lista_de_erros)
         origem_destino_iguais(origem, destino, lista_de_erros)
+        data_ida_maior_que_data_volta(data_ida, data_volta, lista_de_erros)
+        data_ida_menor_data_de_hoje(data_ida, data_pesquisa, lista_de_erros)
         if lista_de_erros is not None:
             for erro in lista_de_erros:
                 mensagem_erro = lista_de_erros[erro]
                 self.add_error(erro, mensagem_erro)
         return self.cleaned_data
+    
